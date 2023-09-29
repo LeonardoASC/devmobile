a46661e5793fff995b86436408794e6af46af50e
LeonardoASC <leonardoaschaves@gmail.com>
2023-09-29 15:48:42 -0300

    feat: outras formas de logar screen login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import { SafeAreaView, View, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Ionicons, EvilIcons } from '@expo/vector-icons';
+import SvgComponent from "../../svg/google";
+import SvgComponentX from "../../svg/x";
+import SvgComponentFace from "../../svg/facebook";
 
 export function Login({ route, navigation }) {
   const [email, setEmail] = useState('');
@@ -13,7 +17,7 @@ export function Login({ route, navigation }) {
   //       email: email,
   //       password: password
   //     });
-  
+
   //     if (response.status === 200) {
   //       // Salve o token em algum lugar seguro, como o AsyncStorage ou Redux, por exemplo
   //       // Por simplicidade, vou apenas navegar para a tela HomePrivate
@@ -36,28 +40,28 @@ export function Login({ route, navigation }) {
         email: email,
         password: password
       });
-  
+
       if (response.status === 200) {
         const token = response.data.access_token;
-        
+
         // Você pode optar por guardar o token para uso posterior aqui
         AsyncStorage.setItem('userToken', token);
-        
+
         // Fazendo chamada subsequente para obter detalhes do usuário
         const userDetails = await axios.get('http://10.55.0.101:8000/api/user', {
           headers: {
             'Authorization': `Bearer ${token}`
           }
         });
-  
+
         // Ajuste esta linha conforme a resposta da sua API. 
         // Estou supondo que a resposta tem uma propriedade "name".
         const userName = userDetails.data.name;
-  
+
         navigation.navigate("HomePrivate", {
           userName: userName
         });
-  
+
       } else {
         Alert.alert('Erro', 'Credenciais inválidas.');
       }
@@ -71,45 +75,128 @@ export function Login({ route, navigation }) {
   };
 
   return (
+    // <SafeAreaView className="flex-1">
+    //   <View className="bg-white h-1/4 rounded-bl-full justify-center items-center">
+    //     <Text className="text-cyan-600 text-xl font-bold text-center">
+    //       Bem-vindo ao Login
+    //     </Text>
+    //   </View>
+
+    //   <View className="p-5 flex justify-center h-2/4 w-full rounded-tr-xl bottom-10">
+
+    //     <Text className="text-white text-3xl font-extrabold">Entrar...</Text>
+    //     <Text className="text-white text-center top-4">Informe suas credenciais.</Text>
+    //     <View className="mt-5 w-full">
+
+    //       <View className="flex-row items-center bg-[#06b6d4] p-2 rounded mb-4 border-b border-zinc-300">
+    //         <Ionicons name="at" size={19} color="white" />
+    //         <TextInput
+    //           value={email}
+    //           onChangeText={setEmail}
+    //           placeholder="Email ID"
+    //           className="flex-1 px-1"
+    //         />
+    //       </View>
+
+    //       <View className="flex-row items-center bg-[#06b6d4] py-2 px-1 rounded mb-4 border-b border-zinc-300">
+    //         <EvilIcons name="lock" size={26} color="white" />
+    //         <TextInput
+    //           value={password}
+    //           onChangeText={setPassword}
+    //           placeholder="Senha"
+    //           secureTextEntry={true}
+    //           className="flex-1 px-1"
+    //         />
+    //         <Text className="text-slate-100">Esqueceu?</Text>
+    //       </View>
+    //     </View>
+    //   </View>
+
+    //   <View className="w-full items-center justify-center">
+    //     <TouchableOpacity
+    //       className="bg-white w-[90%] rounded-xl p-3 shadow-md py-4 bottom-28"
+    //       onPress={handleLogin}
+    //     // onPress={() => navigation.navigate("HomePrivate")}
+    //     >
+    //       <Text className="text-cyan-500 text-center font-bold text-lg ">Entrar</Text>
+    //     </TouchableOpacity>
+
+    //   <Text className="text-center text-slate-200 bottom-20">Ou, Logar com... </Text>
+
+    //     <View className="flex flex-row gap-x-8 mt-4 bottom-16">
+    //       <View className="bg-white w-10 items-center justify-center rounded-xl p-2 px-10 shadow-xl">
+    //         <SvgComponent />
+    //       </View>
+    //       <View className="bg-white w-10 items-center justify-center rounded-xl p-2 px-10 shadow-xl">
+    //         <SvgComponentX />
+    //       </View>
+    //       <View className="bg-white w-10 items-center justify-center rounded-xl p-2 px-10 shadow-xl">
+    //         <SvgComponentFace />
+    //       </View>
+    //     </View>
+    //     <Text className="text-center text-slate-200 ">Novo no aplicativo? Registre</Text>
+    //   </View>
+
+    // </SafeAreaView>
+
     <SafeAreaView className="flex-1">
-      <View className="bg-white h-1/4 rounded-bl-full justify-center items-center">
+      <View className="bg-white flex h-1/4 justify-center items-center rounded-bl-full">
         <Text className="text-cyan-600 text-xl font-bold text-center">
           Bem-vindo ao Login
         </Text>
       </View>
 
-      <View className="p-5 flex justify-center items-center h-2/4 w-full mt-10 rounded-tr-xl">
-        <Text className="text-white">Por favor, insira suas credenciais</Text>
+      <View className="flex-1 p-5 mt-2">
+        <Text className="text-white text-3xl font-extrabold self-center">Entrar...</Text>
+        <Text className="text-white text-center mt-4">Informe suas credenciais.</Text>
 
         <View className="mt-5 w-full">
-          <Text className="text-white mb-2">Email:</Text>
-          <TextInput
-            value={email}
-            onChangeText={setEmail}
-            placeholder="Email"
-            className="bg-white p-2 rounded mb-4"
-          />
+          <View className="flex-row items-center bg-[#06b6d4] p-2 rounded mb-4 border-b border-zinc-300">
+            <Ionicons name="at" size={19} color="white" />
+            <TextInput
+              value={email}
+              onChangeText={setEmail}
+              placeholder="Email ID"
+              className="flex-1 ml-2"
+            />
+          </View>
 
-          <Text className="text-white mb-2">Senha:</Text>
-          <TextInput
-            value={password}
-            onChangeText={setPassword}
-            placeholder="Senha"
-            secureTextEntry={true}
-            className="bg-white p-2 rounded mb-4"
-          />
+          <View className="flex-row items-center bg-[#06b6d4] py-2 px-1 rounded mb-4 border-b border-zinc-300">
+            <EvilIcons name="lock" size={26} color="white" />
+            <TextInput
+              value={password}
+              onChangeText={setPassword}
+              placeholder="Senha"
+              secureTextEntry={true}
+              className="flex-1 ml-2"
+            />
+            <Text className="text-slate-100 ml-2">Esqueceu?</Text>
+          </View>
         </View>
-      </View>
 
-      <View className="w-full items-center justify-center">
         <TouchableOpacity
-          className="bg-white rounded p-3 shadow-md"
+          className="bg-white w-11/12 rounded-xl p-3 shadow-md py-4 self-center mt-5"
           onPress={handleLogin}
-        // onPress={() => navigation.navigate("HomePrivate")}
         >
-          <Text className="text-cyan-500 font-bold text-lg">Entrar</Text>
+          <Text className="text-cyan-500 text-center font-bold text-lg">Entrar</Text>
         </TouchableOpacity>
+
+        <Text className="text-center text-slate-200 mt-8">Ou, Logar com...</Text>
+
+        <View className="flex flex-row justify-center gap-x-8 mt-8">
+          <View className="bg-white w-16 h-16 items-center justify-center rounded-xl p-2 shadow-xl">
+            <SvgComponent />
+          </View>
+          <View className="bg-white w-16 h-16 items-center justify-center rounded-xl p-2 shadow-xl">
+            <SvgComponentX />
+          </View>
+          <View className="bg-white w-16 h-16 items-center justify-center rounded-xl p-2 shadow-xl">
+            <SvgComponentFace />
+          </View>
+        </View>
+        <Text className="text-center text-slate-200 mt-5">Novo no aplicativo? Registre</Text>
       </View>
     </SafeAreaView>
+
   );
 }
