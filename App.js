71ea76bce54fba92994949ac35096fa53d02675d
LeonardoASC <leonardoaschaves@gmail.com>
2023-12-13 13:39:52 -0300

    login automatico apos registro

--- App.js
+++ App.js
@@ -4,68 +4,8 @@ import React, { useEffect } from 'react';
 import { Routes } from "./src/routes";
 import { AuthProvider } from "./src/context/AuthContext"
 
-// import messaging from '@react-native-firebase/messaging';
-// import api from './src/services/api';
 
 export default function App() {
-
-  // const requestUserPermission = async () => {
-  //   const authStatus = await messaging().requestPermission();
-  //   const enabled =
-  //     authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-  //     authStatus === messaging.AuthorizationStatus.PROVISIONAL;
-
-  //   if (enabled) {
-  //     // console.log('Authorization status:', authStatus);
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   if (requestUserPermission()) {
-  //     // retorna o token do dispositivo
-  //     messaging().getToken().then(token => {
-  //         // console.log(token);
-         
-  //     })
-  //   } 
-  //   else {
-  //     console.log("falhou o token man", authStatus);
-  //   }
-
-  //   // Check whether an initial notification is available
-  //   messaging()
-  //     .getInitialNotification()
-  //     .then(async(remoteMessage) => {
-  //       if (remoteMessage) {
-  //         console.log(
-  //           'Notification caused app to open from quit state:',
-  //           remoteMessage.notification,
-  //         );
-  //       }
-  //     });
-
-  //   // Assume a message-notification contains a "type" property in the data payload of the screen to open
-
-  //   messaging().onNotificationOpenedApp(async (remoteMessage) => {
-  //     console.log(
-  //       'Notification caused app to open from background state:',
-  //       remoteMessage.notification,
-  //     );
-  //   });
-
-  //   // Register background handler
-  //   messaging().setBackgroundMessageHandler(async remoteMessage => {
-  //     console.log('Message handled in the background!', remoteMessage);
-  //   });
-
-  //   const unsubscribe = messaging().onMessage(async remoteMessage => {
-  //     Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
-  //   });
-
-  //   return unsubscribe;
-
-  // }, [])
-
   return (
     <AuthProvider>
       <Routes />
--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { SafeAreaView, View, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
 import api from "../../services/api"
 import { Ionicons, EvilIcons } from '@expo/vector-icons';
 import { Pressable } from 'react-native';
+import { AuthContext } from '../../context/AuthContext';
 
 
 export function Register({ route, navigation }) {
@@ -10,6 +11,7 @@ export function Register({ route, navigation }) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+    const { register } = useContext(AuthContext);
 
     const handleRegister = () => {
         // 1. Verificar se os campos não estão vazios
@@ -33,105 +35,115 @@ export function Register({ route, navigation }) {
 
 
         // 4. Enviar os dados para o servidor utilizando Axios:
-        api.post('/register', {
-            name: username,
-            email: email,
-            password: password
-        })
-            .then(response => {
-                if (response.data) {
-                    // alert('Você foi cadastrado!'+' Hora de agendar seu próximo corte e ficar no estilo.');
-                    Alert.alert("Você foi cadastrado!", "Hora de agendar seu próximo corte e ficar no estilo.");
-                    navigation.navigate('Home');
-                } else {
-                    alert('Erro ao registrar! ' + (response.data.message || ''));
-                }
-            })
-            .catch(error => {
-                console.error("Erro na requisição:", error);
-                alert('Ocorreu um erro. Tente novamente mais tarde.');
-            });
+        // api.post('/register', {
+        //     name: username,
+        //     email: email,
+        //     password: password
+        // })
+        //     .then(response => {
+        //         if (response.data) {
+        //             // alert('Você foi cadastrado!'+' Hora de agendar seu próximo corte e ficar no estilo.');
+        //             Alert.alert("Você foi cadastrado!", "Hora de agendar seu próximo corte e ficar no estilo.");
+        //             navigation.navigate('Home');
+        //         } else {
+        //             alert('Erro ao registrar! ' + (response.data.message || ''));
+        //         }
+        //     })
+        //     .catch(error => {
+        //         console.error("Erro na requisição:", error);
+        //         alert('Ocorreu um erro. Tente novamente mais tarde.');
+        //     });
 
-    };
+        register(username, email, password).then(() => {
+            Alert.alert("Você foi cadastrado!", "Hora de agendar seu próximo corte e ficar no estilo.");
+            navigation.navigate('Home');
+        }).catch(error => {
+            console.error("Erro na requisição:", error);
+            alert('Ocorreu um erro. Tente novamente mais tarde.');
+        });
+    
+
+
+};
 
 
 
-    return (
-      
-        <SafeAreaView className="flex-1">
-            <View className="bg-white flex h-1/4 justify-center items-center rounded-bl-full">
-                <Text className="text-cyan-600 text-xl font-bold text-center">
-                    Bem-vindo ao Registro
-                </Text>
-            </View>
+return (
 
-            <View className="flex-1 p-5 mt-2">
-                <Text className="text-white text-3xl font-extrabold self-center">Registrar...</Text>
-                <Text className="text-white text-center mt-4">Por favor, insira suas informações.</Text>
+    <SafeAreaView className="flex-1">
+        <View className="bg-white flex h-1/4 justify-center items-center rounded-bl-full">
+            <Text className="text-cyan-600 text-xl font-bold text-center">
+                Bem-vindo ao Registro
+            </Text>
+        </View>
 
-                <View className="mt-5 w-full">
-                    <View className="flex-row items-center p-2 rounded mb-4 border-b border-zinc-300">
-                        <Ionicons name="person-add-outline" size={20} color="white" />
-                        <TextInput
-                            value={username}
-                            onChangeText={setUsername}
-                            placeholder="Nome de usuário"
-                            placeholderTextColor="#a3a3a3"
-                            className="flex-1 ml-2 text-white"
-                        />
-                    </View>
+        <View className="flex-1 p-5 mt-2">
+            <Text className="text-white text-3xl font-extrabold self-center">Registrar...</Text>
+            <Text className="text-white text-center mt-4">Por favor, insira suas informações.</Text>
 
-                    <View className="flex-row items-center p-2 rounded mb-4 border-b border-zinc-300">
-                        <Ionicons name="at" size={19} color="white" />
-                        <TextInput
-                            value={email}
-                            onChangeText={setEmail}
-                            placeholder="Email"
-                            placeholderTextColor="#a3a3a3"
-                            className="flex-1 ml-2 text-white"
-                        />
-                    </View>
-
-                    <View className="flex-row items-center py-2 px-1 rounded mb-4 border-b border-zinc-300">
-                        <EvilIcons name="lock" size={26} color="white" />
-                        <TextInput
-                            value={password}
-                            onChangeText={setPassword}
-                            placeholder="Senha"
-                            placeholderTextColor="#a3a3a3"
-                            secureTextEntry={true}
-                            className="flex-1 ml-2 text-white"
-                        />
-                    </View>
-
-                    <View className="flex-row items-center py-2 px-1 rounded mb-4 border-b border-zinc-300">
-                        <EvilIcons name="lock" size={26} color="white" />
-                        <TextInput
-                            value={confirmPassword}
-                            onChangeText={setConfirmPassword}
-                            placeholder="Confirme a Senha"
-                            placeholderTextColor="#a3a3a3"
-                            secureTextEntry={true}
-                            className="flex-1 ml-2 text-white"
-                        />
-                    </View>
+            <View className="mt-5 w-full">
+                <View className="flex-row items-center p-2 rounded mb-4 border-b border-zinc-300">
+                    <Ionicons name="person-add-outline" size={20} color="white" />
+                    <TextInput
+                        value={username}
+                        onChangeText={setUsername}
+                        placeholder="Nome de usuário"
+                        placeholderTextColor="#a3a3a3"
+                        className="flex-1 ml-2 text-white"
+                    />
                 </View>
 
-                <TouchableOpacity
-                    className="bg-white w-11/12 rounded-xl p-3 shadow-md py-4 self-center mt-5"
-                    onPress={handleRegister}
-                >
-                    <Text className="text-cyan-500 text-center font-bold text-lg">Registrar</Text>
-                </TouchableOpacity>
+                <View className="flex-row items-center p-2 rounded mb-4 border-b border-zinc-300">
+                    <Ionicons name="at" size={19} color="white" />
+                    <TextInput
+                        value={email}
+                        onChangeText={setEmail}
+                        placeholder="Email"
+                        placeholderTextColor="#a3a3a3"
+                        className="flex-1 ml-2 text-white"
+                    />
+                </View>
 
-                <View className=" flex flex-row justify-center mt-8">
-                    <Text className="text-white">Já tem uma conta?</Text>
-                    <Pressable onPress={() => navigation.navigate('Login')}>
-                        <Text className="text-white underline ml-2">Entrar</Text>
-                    </Pressable>
+                <View className="flex-row items-center py-2 px-1 rounded mb-4 border-b border-zinc-300">
+                    <EvilIcons name="lock" size={26} color="white" />
+                    <TextInput
+                        value={password}
+                        onChangeText={setPassword}
+                        placeholder="Senha"
+                        placeholderTextColor="#a3a3a3"
+                        secureTextEntry={true}
+                        className="flex-1 ml-2 text-white"
+                    />
+                </View>
+
+                <View className="flex-row items-center py-2 px-1 rounded mb-4 border-b border-zinc-300">
+                    <EvilIcons name="lock" size={26} color="white" />
+                    <TextInput
+                        value={confirmPassword}
+                        onChangeText={setConfirmPassword}
+                        placeholder="Confirme a Senha"
+                        placeholderTextColor="#a3a3a3"
+                        secureTextEntry={true}
+                        className="flex-1 ml-2 text-white"
+                    />
                 </View>
             </View>
-        </SafeAreaView>
 
-    );
+            <TouchableOpacity
+                className="bg-white w-11/12 rounded-xl p-3 shadow-md py-4 self-center mt-5"
+                onPress={handleRegister}
+            >
+                <Text className="text-cyan-500 text-center font-bold text-lg">Registrar</Text>
+            </TouchableOpacity>
+
+            <View className=" flex flex-row justify-center mt-8">
+                <Text className="text-white">Já tem uma conta?</Text>
+                <Pressable onPress={() => navigation.navigate('Login')}>
+                    <Text className="text-white underline ml-2">Entrar</Text>
+                </Pressable>
+            </View>
+        </View>
+    </SafeAreaView>
+
+);
 }
